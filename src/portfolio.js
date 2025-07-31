ef4947762311ba0ef6d9f38f15b673e75c0ff8d9
ziauddin14 <ziamaryam2526@gmail.com>
2025-07-31 11:18:48 +0500

    Subtract Education part

--- src/portfolio.js
+++ src/portfolio.js
@@ -103,7 +103,7 @@ https://fontawesome.com/icons?d=gallery */
 // Education Section
 
 const educationInfo = {
-  display: true, // Set false to hide this section, defaults to true
+  display: false, // Set false to hide this section, defaults to true
   schools: [
     {
       schoolName: "Harvard University",
